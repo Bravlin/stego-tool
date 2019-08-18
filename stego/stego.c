07689de5d95b51ccf4bc6fb53e570a84e13e182c
Bravlin <bravlinleonel@gmail.com>
2019-08-18 18:01:33 -0300

    Code refactoring

--- stego/stego.c
+++ stego/stego.c
@@ -1,19 +1,19 @@
 #include "stego.h"
 
 #define BITS8 0b11111111
+#define LAST_BIT 0b00000001
 #define CHAR_SIZE 8
 
-int hide_text(uint32_t* pixels, unsigned int pixel_size, uint32_t x, uint32_t y, uint8_t k, unsigned char* text)
+int hide_text(uint32_t* pixels, unsigned int pixel_size, uint32_t x, uint32_t y, uint8_t k,
+        unsigned char* text)
 {
     int8_t i;
-    uint8_t shift;
-    uint8_t pixels_per_char;
-    uint8_t rest;
+    //uint8_t pixels_per_char;
     int64_t to_hide = -1;
-    unsigned int pixel = 0;
-    unsigned long int text_size = (strlen(text) + 1) * CHAR_SIZE;
-    unsigned long int total_pixels = x * y;
-    unsigned long int usable_pixels = total_pixels - 3;
+    unsigned long int pixel = 0;
+    unsigned long int text_size_chars = (strlen(text) + 1);
+    unsigned long int text_size_bits = text_size_chars * CHAR_SIZE;
+    unsigned long int usable_pixels = x * y - 3;
 
     if (k < 1 || k > pixel_size)
         return 0; // Check for a proper return code later
@@ -24,10 +24,10 @@ int hide_text(uint32_t* pixels, unsigned int pixel_size, uint32_t x, uint32_t y,
 
     // Checks if k bits per pixel are enough.
     // If not, then calculate an appropiate minimum size for k
-    if (usable_pixels * k < text_size)
-        k = (text_size % usable_pixels == 0) ? text_size / usable_pixels : text_size / usable_pixels + 1;
+    if (usable_pixels * k < text_size_bits)
+        k = text_size_bits / usable_pixels + (text_size_bits % usable_pixels != 0);
 
-    // Checks if more bits than what is possible to hide are needed 
+    // Checks if more bits than what is intended to hide per pixel are needed 
     if (k > CHAR_SIZE)
         return 0;
 
@@ -35,45 +35,46 @@ int hide_text(uint32_t* pixels, unsigned int pixel_size, uint32_t x, uint32_t y,
     for (i = 2; i >= 0; i--)
     {
         pixels[pixel] &= 0b11111110;
-        pixels[pixel] |= ((k - 1) >> i) & 0b00000001;
+        pixels[pixel] |= ((k - 1) >> i) & LAST_BIT;
         pixel++; 
     }
 
-    pixels_per_char = CHAR_SIZE / k;
-    rest = CHAR_SIZE % k;
+    //pixels_per_char = CHAR_SIZE / k;
     do
     {
         to_hide++;
-        shift = CHAR_SIZE - k;
-        for (i = pixels_per_char - 1; i >= 0; i--)
-        {
-            pixels[pixel] &= BITS8 << k;
-            pixels[pixel] |= (text[to_hide] >> shift) & ~(BITS8 << k);
-            pixel++;
-            shift -= k;
-        }
-        if (rest > 0)
-        {
-            pixels[pixel] &= BITS8 << rest;
-            pixels[pixel] |= text[to_hide] & ~(BITS8 << rest);
-            pixel++;
-        } 
+        hide_char(text[to_hide], k, pixels, &pixel);
     }
     while (text[to_hide] != '\0');
 
     return 1;
 }
 
+void hide_char(unsigned char c, uint8_t k, uint32_t* pixels, unsigned long int* pixel)
+{
+    uint8_t shift = CHAR_SIZE;
+    while (shift > k)
+    {
+        shift -= k;
+        pixels[*pixel] &= BITS8 << k;
+        pixels[*pixel] |= (c >> shift) & ~(BITS8 << k);
+        (*pixel)++;
+    }
+    if (shift > 0)
+    {
+        pixels[*pixel] &= BITS8 << shift;
+        pixels[*pixel] |= c & ~(BITS8 << shift);
+        (*pixel)++;
+    }
+}
+
 int get_text(uint32_t* pixels, unsigned char* text)
 {
     int8_t i;
-    uint8_t shift;
-    uint8_t mask;
-    uint8_t pixels_per_char;
-    uint8_t rest;
+    //uint8_t pixels_per_char;
     uint8_t k = 0;
     int64_t to_get = -1;
-    unsigned int pixel = 0;
+    unsigned long int pixel = 0;
 
     if (pixels == NULL)
         return 0;
@@ -81,7 +82,7 @@ int get_text(uint32_t* pixels, unsigned char* text)
     // Recovers k from the first 3 pixels
     for (i = 2; i >= 0; i--)
     {
-        k |= (pixels[pixel] & 0b00000001) << i;
+        k |= (pixels[pixel] & LAST_BIT) << i;
         pixel++; 
     }
     k++;
@@ -90,28 +91,43 @@ int get_text(uint32_t* pixels, unsigned char* text)
     if (k > 8)
         return 0;
 
-    pixels_per_char = CHAR_SIZE / k;
-    rest = CHAR_SIZE % k;
+    //pixels_per_char = CHAR_SIZE / k;
     do
     {
         to_get++;
-        mask = BITS8;
-        text[to_get] &= ~(mask);
-        shift = CHAR_SIZE - k;
-        for (i = pixels_per_char - 1; i >= 0; i--)
-        {
-            text[to_get] |= (pixels[pixel] << shift) & mask; 
-            pixel++;
-            shift -= k;
-            mask >>= k;
-        }
-        if (rest > 0)
-        {
-            text[to_get] |= pixels[pixel] & ~(BITS8 << rest);
-            pixel++;
-        }
+        get_char(pixels, k, &(text[to_get]), &pixel);
     }
     while (text[to_get] != '\0');
 
     return 1;
 }
+
+void get_char(uint32_t* pixels, uint8_t k, unsigned char* c, unsigned long int* pixel)
+{
+    uint8_t mask = BITS8;
+    uint8_t shift = CHAR_SIZE;
+    *c &= ~(mask);
+    while (shift > k)
+    {
+        shift -= k;
+        *c |= (pixels[*pixel] << shift) & mask;
+        mask >>= k;
+        (*pixel)++;
+    }
+    if (shift > 0)
+    {
+        *c |= pixels[*pixel] & ~(BITS8 << shift);
+        (*pixel)++;
+    }
+}
+
+/*
+unsigned long int padding_jump(uint32_t* pixels, unsigned char last_char, uint8_t k,
+        unsigned long int next_pixel)
+{
+    unsigned char xor = last_char ^ pixels[next_pixel];
+    if ((xor >> (k - 1)) & LAST_BIT)
+        return (xor * k);
+    return 0;
+}
+*/
--- stego/stego.h
+++ stego/stego.h
@@ -3,4 +3,6 @@
 #include <string.h>
 
 int hide_text(uint32_t*, unsigned int, uint32_t, uint32_t, uint8_t, unsigned char*);
-int get_text(uint32_t* pixels, unsigned char* text);
+void hide_char(unsigned char, uint8_t, uint32_t*, unsigned long int*);
+int get_text(uint32_t*, unsigned char*);
+void get_char(uint32_t*, uint8_t, unsigned char*, unsigned long int*);
