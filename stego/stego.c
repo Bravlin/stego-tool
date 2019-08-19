141ce1280f4eb919eb972d8b6347f7e297ed01e4
Bravlin <bravlinleonel@gmail.com>
2019-08-19 13:07:19 -0300

    Padding implementation

--- stego/stego.c
+++ stego/stego.c
@@ -3,23 +3,23 @@
 #define BITS8 0b11111111
 #define LAST_BIT 0b00000001
 #define CHAR_SIZE 8
+#define MAX_TEXT_SIZE 256
 
-int hide_text(uint32_t* pixels, unsigned int pixel_size, uint32_t x, uint32_t y, uint8_t k,
-        unsigned char* text)
+int hide_text(const unsigned char* text, unsigned int pixel_size, uint32_t x, uint32_t y,
+        uint8_t k, uint32_t* pixels)
 {
     int8_t i;
-    //uint8_t pixels_per_char;
-    int64_t to_hide = -1;
+    unsigned long int padding_left;
+    uint8_t pixels_per_char;
+    unsigned long int jump;
+    uint8_t to_hide = 0;
     unsigned long int pixel = 0;
-    unsigned long int text_size_chars = (strlen(text) + 1);
-    unsigned long int text_size_bits = text_size_chars * CHAR_SIZE;
+    unsigned long int text_size_chars = strlen(text);
+    unsigned long int text_size_bits = (text_size_chars + 1) * CHAR_SIZE;
     unsigned long int usable_pixels = x * y - 3;
 
-    if (k < 1 || k > pixel_size)
-        return 0; // Check for a proper return code later
-    if (pixels == NULL)
-        return 0;
-    if (text == NULL)
+    if (k < 1 || k > pixel_size || pixels == NULL || text == NULL
+            || text_size_chars > MAX_TEXT_SIZE)
         return 0;
 
     // Checks if k bits per pixel are enough.
@@ -27,7 +27,7 @@ int hide_text(uint32_t* pixels, unsigned int pixel_size, uint32_t x, uint32_t y,
     if (usable_pixels * k < text_size_bits)
         k = text_size_bits / usable_pixels + (text_size_bits % usable_pixels != 0);
 
-    // Checks if more bits than what is intended to hide per pixel are needed 
+    // Checks if more bits than what is allowed to hide per pixel are needed 
     if (k > CHAR_SIZE)
         return 0;
 
@@ -39,13 +39,22 @@ int hide_text(uint32_t* pixels, unsigned int pixel_size, uint32_t x, uint32_t y,
         pixel++; 
     }
 
-    //pixels_per_char = CHAR_SIZE / k;
-    do
+    // Hides the text length as the first character
+    hide_char(text_size_chars, k, pixels, &pixel);
+
+    pixels_per_char = CHAR_SIZE / k + (CHAR_SIZE % k != 0);
+    padding_left = usable_pixels - (text_size_chars + 1) * pixels_per_char;
+    while (text[to_hide] != '\0')
     {
-        to_hide++;
+        if (padding_left > 0)
+        {
+            jump = padding_jump(pixels[pixel - 1], k, padding_left);
+            pixel += jump;
+            padding_left -= jump;
+        }
         hide_char(text[to_hide], k, pixels, &pixel);
+        to_hide++;
     }
-    while (text[to_hide] != '\0');
 
     return 1;
 }
@@ -60,23 +69,25 @@ void hide_char(unsigned char c, uint8_t k, uint32_t* pixels, unsigned long int* 
         pixels[*pixel] |= (c >> shift) & ~(BITS8 << k);
         (*pixel)++;
     }
-    if (shift > 0)
-    {
-        pixels[*pixel] &= BITS8 << shift;
-        pixels[*pixel] |= c & ~(BITS8 << shift);
-        (*pixel)++;
-    }
+    pixels[*pixel] &= BITS8 << shift;
+    pixels[*pixel] |= c & ~(BITS8 << shift);
+    (*pixel)++;
 }
 
-int get_text(uint32_t* pixels, unsigned char* text)
+int get_text(const uint32_t* pixels, uint32_t x, uint32_t y, unsigned char* text)
 {
     int8_t i;
-    //uint8_t pixels_per_char;
+    uint8_t to_get;
+    uint8_t text_size_chars;
+    uint8_t pixels_per_char;
+    unsigned char last_char;
+    unsigned long int jump;
+    unsigned long int padding_left;
     uint8_t k = 0;
-    int64_t to_get = -1;
     unsigned long int pixel = 0;
+    unsigned long int usable_pixels = x * y - 3;
 
-    if (pixels == NULL)
+    if (pixels == NULL || text == NULL)
         return 0;
 
     // Recovers k from the first 3 pixels
@@ -87,22 +98,27 @@ int get_text(uint32_t* pixels, unsigned char* text)
     }
     k++;
 
-    // Checks for an impossible value for k
-    if (k > 8)
-        return 0;
+    // Recovers the text size
+    get_char(pixels, k, &text_size_chars, &pixel);
 
-    //pixels_per_char = CHAR_SIZE / k;
-    do
+    pixels_per_char = CHAR_SIZE / k + (CHAR_SIZE % k != 0);
+    padding_left = usable_pixels - (text_size_chars + 1) * pixels_per_char;
+    for (to_get = 0; to_get < text_size_chars; to_get++)
     {
-        to_get++;
+        if (padding_left > 0)
+        {
+            jump = padding_jump(pixels[pixel - 1], k, padding_left);
+            pixel += jump;
+            padding_left -= jump;
+        }
         get_char(pixels, k, &(text[to_get]), &pixel);
     }
-    while (text[to_get] != '\0');
+    text[to_get] = '\0';
 
     return 1;
 }
 
-void get_char(uint32_t* pixels, uint8_t k, unsigned char* c, unsigned long int* pixel)
+void get_char(const uint32_t* pixels, uint8_t k, unsigned char* c, unsigned long int* pixel)
 {
     uint8_t mask = BITS8;
     uint8_t shift = CHAR_SIZE;
@@ -114,20 +130,21 @@ void get_char(uint32_t* pixels, uint8_t k, unsigned char* c, unsigned long int* 
         mask >>= k;
         (*pixel)++;
     }
-    if (shift > 0)
-    {
-        *c |= pixels[*pixel] & ~(BITS8 << shift);
-        (*pixel)++;
-    }
+    *c |= pixels[*pixel] & ~(BITS8 << shift);
+    (*pixel)++;
 }
 
-/*
-unsigned long int padding_jump(uint32_t* pixels, unsigned char last_char, uint8_t k,
-        unsigned long int next_pixel)
+// Calculates a pseudo-random jump
+unsigned long int padding_jump(uint32_t prev_pixel, uint8_t k, unsigned long int padding_left)
 {
-    unsigned char xor = last_char ^ pixels[next_pixel];
-    if ((xor >> (k - 1)) & LAST_BIT)
-        return (xor * k);
-    return 0;
+    unsigned char xor = padding_left ^ prev_pixel;
+    unsigned long int jump;
+    if (xor % 2 == 1)
+    {
+        jump = xor % (CHAR_SIZE * k) + 1;
+        jump %= padding_left + 1;
+    }
+    else
+        jump = 0;
+    return jump;
 }
-*/
--- stego/stego.h
+++ stego/stego.h
@@ -2,7 +2,8 @@
 #include <stdint.h>
 #include <string.h>
 
-int hide_text(uint32_t*, unsigned int, uint32_t, uint32_t, uint8_t, unsigned char*);
+int hide_text(const unsigned char*, unsigned int, uint32_t, uint32_t, uint8_t, uint32_t*);
 void hide_char(unsigned char, uint8_t, uint32_t*, unsigned long int*);
-int get_text(uint32_t*, unsigned char*);
-void get_char(uint32_t*, uint8_t, unsigned char*, unsigned long int*);
+int get_text(const uint32_t*, uint32_t, uint32_t, unsigned char*);
+void get_char(const uint32_t*, uint8_t, unsigned char*, unsigned long int*);
+unsigned long int padding_jump(uint32_t, uint8_t, unsigned long int);
