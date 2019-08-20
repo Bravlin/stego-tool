23c19fd5710b94b1805379b6bd9bcf17a77c465a
SebastianCanonaco <sebastiancanonaco@gmail.com>
2019-08-20 20:16:08 -0300

    Merge with other branchs

--- measure.c
+++ measure.c
@@ -2,6 +2,9 @@
 #include <math.h>
 #include "measure.h"
 #define MAX_PIXEL_VALUE 0b111111111111111111111111
+#define k1 0.01
+#define k2 0.03
+#define L 255
 
 /* Mean Squared Error */
 float mse(uint32_t *original_image, uint32_t *stego_image, uint32_t width, uint32_t heigth)
@@ -42,7 +45,37 @@ float psnr(uint32_t *original_image, uint32_t *stego_image, uint32_t width, uint
 /* Structural Similarity */
 float ssim(uint32_t *original_image, uint32_t *stego_image, uint32_t width, uint32_t heigth, uint32_t window_size)
 {
-    float luminance, constrast, structure;
-    float return_value;
+    float return_value, luminance, constrast, structure, c1, c2, c3, mu_x = 0, mu_y = 0, var_x = 0, var_y = 0, covar_xy = 0;
+    uint32_t i, image_size = width * heigth;
+
+    c1 = pow(k1 * L, 2);
+    c2 = pow(k2 * L, 2);
+    c3 = c2 / 2;
+
+    /* First loop to calculate de mean of each image */
+    for (i = 0; i < image_size; i++)
+    {
+        mu_x += original_image[i];
+        mu_y += stego_image[i];
+    }
+    mu_x /= image_size;
+    mu_y /= image_size;
+
+    /* Second loop to calculate variance and covariance */
+    for (i = 0; i < image_size; i++)
+    {
+        var_x += pow(original_image[i] - mu_x, 2);
+        var_y += pow(stego_image[i] - mu_y, 2);
+        covar_xy += (original_image[i] - mu_x) * (stego_image[i] - mu_y);
+    }
+    var_x = sqrt(var_x / (image_size - 1));
+    var_y = sqrt(var_y / (image_size - 1));
+    covar_xy /= image_size;
+
+    luminance = (2 * mu_x * mu_y + c1) / (pow(mu_x, 2) + pow(mu_y, 2) + c1);
+    constrast = (2 * var_x * var_y + c2) / (pow(var_x, 2) + pow(var_y, 2) + c2);
+    structure = (covar_xy + c3) / (var_x * var_y + c3);
+
+    return_value = luminance * constrast * structure;
     return return_value;
 }
