7fec8a1165fdca825643024965cfc9c35378d523
Bravlin <bravlinleonel@gmail.com>
2019-08-22 17:20:37 -0300

    General polish

--- stego/stego.h
+++ stego/stego.h
@@ -2,8 +2,5 @@
 #include <stdint.h>
 #include <string.h>
 
-int hide_text(const unsigned char*, unsigned int, uint32_t, uint32_t, uint8_t, uint32_t*);
-void hide_char(unsigned char, uint8_t, uint32_t*, unsigned long int*);
+int hide_text(const unsigned char*, uint32_t, uint32_t, uint8_t, uint32_t*);
 int get_text(const uint32_t*, uint32_t, uint32_t, unsigned char*);
-void get_char(const uint32_t*, uint8_t, unsigned char*, unsigned long int*);
-unsigned long int padding_jump(uint32_t, uint8_t, unsigned long int);
