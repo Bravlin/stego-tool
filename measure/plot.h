957b19086247955b85e8afa2510e2b78b4d87d03
SebastianCanonaco <sebastiancanonaco@gmail.com>
2019-08-22 18:28:01 -0300

    change float to double

--- measure/plot.h
+++ measure/plot.h
@@ -1,1 +1,1 @@
-void plot(float *x_value, int *y_value, int count, char *x_label, char *y_label, char *title, char *output_filename);
+void plot(double *x_value, int *y_value, int count, char *x_label, char *y_label, char *title, char *output_filename);
