a04871d13ecf10d20a65f279c7ce807d86001d83
Franco Lanzillotta <franco.lanzi96@gmail.com>
2019-08-22 18:13:01 -0300

    Some changes

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 #include "bitmap/bitmap.h"
 #include "measure/measure.h"
 
+#define IMG_MAX_SIZE 2073600
+
 void copy_pixels(uint32_t *pixels, uint32_t *copy, uint32_t x, uint32_t y);
 
 int main(int argc, char **argv)
 {
-    uint32_t pixels[2073600];
+    uint32_t *pixels = (uint32_t *) malloc(sizeof(uint32_t) * IMG_MAX_SIZE);
     uint32_t x, y;
     uint16_t pixel_size;
     char action = argv[1][0];
@@ -23,10 +25,10 @@ int main(int argc, char **argv)
     {
         char *text = argv[3];
         uint8_t k = argv[4][0] - '0';
-        uint32_t old_pixels[2073600];
+        uint32_t *old_pixels = (uint32_t *) malloc(sizeof(uint32_t) * IMG_MAX_SIZE);
         get_pixels(img_file, pixels, &x, &y, &pixel_size);
         copy_pixels(pixels, old_pixels, x, y);
-        hide_text(text, pixel_size, x, y, k, pixels);
+        hide_text(text, x, y, k, pixels);
         set_pixels(img_file, pixels);
         printf("MSE = %f\n", mse(old_pixels, pixels, x, y));
         printf("PSNR = %f\n", psnr(old_pixels, pixels, x, y));
