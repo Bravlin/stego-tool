959069438e1aa519d29b3ad57013cafc98df2abc
SebastianCanonaco <sebastiancanonaco@gmail.com>
2019-08-25 14:29:16 -0300

    pixel size implemented is psnr

--- measure/plot.c
+++ measure/plot.c
@@ -29,7 +29,7 @@ void plot(long double *x_value, int *y_value, int count, char *x_label, char *y_
 
     for (i = 0; i < count; i++)
     {
-        fprintf(temp_file, "%lf %d \n", x_value[i], y_value[i]);
+        fprintf(temp_file, "%Lf %d \n", x_value[i], y_value[i]);
     }
 
     fclose(temp_file);
